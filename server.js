fcca4d2d70cbe810c43b24a673dc0a2722fd6ed4
cy <cybc00@hotmail.com>
2017-10-27 16:55:49 -0700

    index.html relocation

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use('/static', express.static(path.join(__dirname, "static")));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + "/templates/index.html"));
+  res.sendFile(path.join(__dirname + "/static/index.html"));
 });
 
 app.listen(3000, () => {
